5abc381831fa497cf137405892c7c6d3555ad872
Gerard Gorman <g.gorman@imperial.ac.uk>
2014-07-17 01:04:38 +0100

    Dumped lots of old crud. Tidied up some code.

---
+++ src/CTImage.h
@@ -0,0 +1,119 @@
+/*  Copyright (C) 2010 Imperial College London and others.
+ *
+ *  Please see the AUTHORS file in the main source directory for a
+ *  full list of copyright holders.
+ *
+ *  Gerard Gorman
+ *  Applied Modelling and Computation Group
+ *  Department of Earth Science and Engineering
+ *  Imperial College London
+ *
+ *  g.gorman@imperial.ac.uk
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above
+ *  copyright notice, this list of conditions and the following
+ *  disclaimer in the documentation and/or other materials provided
+ *  with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ *  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ *  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+
+#ifndef CTIMAGE_H
+#define CTIMAGE_H
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Mesh_triangulation_3.h>
+#include <CGAL/Mesh_complex_3_in_triangulation_3.h>
+#include <CGAL/Mesh_criteria_3.h>
+
+#include <CGAL/Labeled_image_mesh_domain_3.h>
+#include <CGAL/make_mesh_3.h>
+#include <CGAL/Image_3.h>
+
+// Domain
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Labeled_image_mesh_domain_3<CGAL::Image_3,K> Mesh_domain;
+
+// Triangulation
+typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
+typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
+
+typedef Tr::Point Point;
+
+// Criteria
+typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
+typedef CGAL::Mesh_constant_domain_field_3<Mesh_domain::R,
+                                           Mesh_domain::Index> Sizing_field;
+
+#include <boost/filesystem.hpp>
+#include "boost/filesystem/fstream.hpp"
+#include "boost/filesystem/operations.hpp"
+
+class CTImage{
+public:
+  CTImage();
+  ~CTImage();
+
+  void verbose_on();
+
+  size_t get_NNodes();
+  size_t get_NElements();
+  size_t get_NFacets();
+
+  int read_raw_ese_image(const char *name, int slab_size);
+
+  void mesh();
+
+  void trim_channels(int in_boundary, int out_boundary);
+
+  // Write INR file.
+  void write_inr();
+
+  // Write NRRD file.
+  void write_nrrd();
+
+  // Write vox file.
+  void write_vox();
+
+  // Write VTK unstructured grid file (*.vtu)
+  void write_vtu();
+
+  // Write GMSH file.
+  int write_gmsh();
+  
+private:
+  double volume(const double *x0, const double *x1, const double *x2, const double *x3) const;
+
+  bool verbose;
+  unsigned char *raw_image;
+  int image_size, dims[3];
+  double resolution;
+  CGAL::Image_3 *image;
+  Mesh_domain *domain;
+  boost::filesystem::path stem;
+
+  std::vector<double> xyz;
+  std::vector<int> tets;
+  std::vector<int> facets;
+  std::vector<int> facet_ids;
+};
+
+#endif
---
+++ src/convert_microct.cpp
@@ -0,0 +1,153 @@
+/*  Copyright (C) 2010 Imperial College London and others.
+ *
+ *  Please see the AUTHORS file in the main source directory for a
+ *  full list of copyright holders.
+ *
+ *  Gerard Gorman
+ *  Applied Modelling and Computation Group
+ *  Department of Earth Science and Engineering
+ *  Imperial College London
+ *
+ *  g.gorman@imperial.ac.uk
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above
+ *  copyright notice, this list of conditions and the following
+ *  disclaimer in the documentation and/or other materials provided
+ *  with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ *  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ *  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <getopt.h>
+
+#include "CTImage.h"
+
+void usage(char *cmd){
+  std::cout<<"Usage: "<<cmd<<" CT-image [options]\n"
+           <<"\nOptions:\n"
+           <<" -h, --help\n\tHelp! Prints this message.\n"
+           <<" -v, --verbose\n\tVerbose output.\n"
+	   <<" -c format, --convert format\n\tConvert image to another format. Options are vox, nrrd.\n"
+           <<" -s width, --slab width\n\tExtract a square block of size 'width' from the data.\n";
+  return;
+}
+
+int parse_arguments(int argc, char **argv,
+                    std::string &filename, bool &verbose, std::string &convert, int &slab_width){
+
+  // Set defaults
+  verbose = false;
+  slab_width = -1;
+
+  if(argc==1){
+    usage(argv[0]);
+    exit(0);
+  }
+
+  struct option longOptions[] = {
+    {"help",    0,                 0, 'h'},
+    {"verbose", 0,                 0, 'v'},
+    {"convert", optional_argument, 0, 'c'},
+    {"slab",    optional_argument, 0, 's'},
+    {0, 0, 0, 0}
+  };
+
+  int optionIndex = 0;
+  int verbosity = 0;
+  int c;
+  const char *shortopts = "hvc:s:";
+
+  // Set opterr to nonzero to make getopt print error messages
+  opterr=1;
+  while (true){
+    c = getopt_long(argc, argv, shortopts, longOptions, &optionIndex);
+    
+    if (c == -1) break;
+    
+    switch (c){
+    case 'h':
+      usage(argv[0]);
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    case 'c':
+      convert = std::string(optarg);
+      break;
+    case 's':
+      slab_width = atoi(optarg);
+      break;    
+    case '?':
+      // missing argument only returns ':' if the option string starts with ':'
+      // but this seems to stop the printing of error messages by getopt?
+      std::cerr<<"ERROR: unknown option or missing argument\n";
+      usage(argv[0]);
+      exit(-1);
+    case ':':
+      std::cerr<<"ERROR: missing argument\n";
+      usage(argv[0]);
+      exit(-1);
+    default:
+      // unexpected:
+      std::cerr<<"ERROR: getopt returned unrecognized character code\n";
+      exit(-1);
+    }
+  }
+
+  filename = std::string(argv[argc-1]);
+
+  return 0;
+}
+
+int main(int argc, char **argv){
+  if(argc==1){
+    usage(argv[0]);
+    exit(-1);
+  }
+    
+  std::string filename, convert;
+  bool verbose, generate_mesh;
+  int slab_width;
+
+  parse_arguments(argc, argv, filename, verbose, convert, slab_width);
+
+  CTImage image;
+  if(verbose)
+    image.verbose_on();
+  
+  image.read_raw_ese_image(filename.c_str(), slab_width);
+  
+  if(convert==std::string("vox")){
+    if(verbose)
+      std::cout<<"INFO: Write VOX file\n";
+
+    image.write_vox();
+  }else if(convert==std::string("nrrd")){
+    if(verbose)
+      std::cout<<"INFO: Write NRRD file\n";
+
+    image.write_nrrd();
+  }
+
+  return 0;
+}
